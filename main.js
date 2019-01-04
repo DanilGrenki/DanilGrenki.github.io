b16192a7acfdeb249c253e9ef38aca0cfa42d431
DanilGrenki <46190654+DanilGrenki@users.noreply.github.com>
2019-01-04 11:50:45 +0300

    добавил октаву

--- main.js
+++ main.js
@@ -26,7 +26,10 @@
 	audio.stop();
 });
 	});
+
 	
+	
+  
 
 	 $( ".key" )
   .mouseup(function(event) {
@@ -96,6 +99,59 @@ $( ".H" )
   .mousedown(function(event) {
   	$('#J').get(0).play().currentTime=0;
   })
+   $( ".J" )
+  .mousedown(function(event) {
+  	$('#J').get(0).play().currentTime=0;
+  })
+
+   $( ".Z" )
+  .mousedown(function(event) {
+  	$('#Z').get(0).play().currentTime=0;
+  })
+   $( ".X" )
+  .mousedown(function(event) {
+  	$('#X').get(0).play().currentTime=0;
+  })
+   $( ".C" )
+  .mousedown(function(event) {
+  	$('#C').get(0).play().currentTime=0;
+  })
+   $( ".V" )
+  .mousedown(function(event) {
+  	$('#V').get(0).play().currentTime=0;
+  })
+   $( ".B" )
+  .mousedown(function(event) {
+  	$('#B').get(0).play().currentTime=0;
+  })
+   $( ".N" )
+  .mousedown(function(event) {
+  	$('#N').get(0).play().currentTime=0;
+  })
+   $( ".M" )
+  .mousedown(function(event) {
+  	$('#M').get(0).play().currentTime=0;
+  })
+   $( ".Q" )
+  .mousedown(function(event) {
+  	$('#Q').get(0).play().currentTime=0;
+  })
+   $( ".E" )
+  .mousedown(function(event) {
+  	$('#E').get(0).play().currentTime=0;
+  })
+   $( ".T" )
+  .mousedown(function(event) {
+  	$('#T').get(0).play().currentTime=0;
+  })
+   $( ".I" )
+  .mousedown(function(event) {
+  	$('#I').get(0).play().currentTime=0;
+  })
+  $( ".P" )
+  .mousedown(function(event) {
+  	$('#P').get(0).play().currentTime=0;
+  })
 
 })
    
@@ -151,5 +207,67 @@ $( ".H" )
   .mousemove(function(event) {
     $('#J').get(0).play().currentTime=0;
   })
-
+  $( ".Z" )
+  .mousemove(function(event) {
+  	$('#Z').get(0).play().currentTime=0;
+  })
+   $( ".X" )
+  .mousemove(function(event) {
+  	$('#X').get(0).play().currentTime=0;
+  })
+   $( ".C" )
+ .mousemove(function(event) {
+  	$('#C').get(0).play().currentTime=0;
+  })
+   $( ".V" )
+  .mousemove(function(event) {
+  	$('#V').get(0).play().currentTime=0;
+  })
+   $( ".B" )
+  .mousemove(function(event) {
+  	$('#B').get(0).play().currentTime=0;
+  })
+   $( ".N" )
+  .mousemove(function(event) {
+  	$('#N').get(0).play().currentTime=0;
+  })
+   $( ".M" )
+  .mousemove(function(event) {
+  	$('#M').get(0).play().currentTime=0;
+  })
+   $( ".Q" )
+  .mousemove(function(event) {
+  	$('#Q').get(0).play().currentTime=0;
+  })
+   $( ".E" )
+  .mousemove(function(event) {
+  	$('#E').get(0).play().currentTime=0;
+  })
+   $( ".T" )
+  .mousemove(function(event) {
+  	$('#T').get(0).play().currentTime=0;
+  })
+   $( ".I" )
+  .mousemove(function(event) {
+  	$('#I').get(0).play().currentTime=0;
+  })
+  $( ".P" )
+  .mousemove(function(event) {
+  	$('#P').get(0).play().currentTime=0;
+  })
 })
+/*
+$ (function() {
+	var key = $(event.target).attr('class');
+    var note =$('#'+key)[0];
+
+    ($(event.target).attr('class');)
+
+$(this).mousedown(function(event){
+
+    note.play();
+    note.currentTime = 0
+})
+});*/
+
+
